a06d4f2c94e19731c3abd16b1432407f0f5785c8
Michael Kimsal <michael@kimsal.com>
2024-11-03 16:22:30 -0500

    feat: add initial main controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/main');
+Route::get('/main', [MainController::class, 'index']);
