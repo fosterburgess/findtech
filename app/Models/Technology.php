ca782639eabbcae9c6a26b3c12cd329be8ecbc54
Michael Kimsal <michael@kimsal.com>
2024-11-03 15:59:16 -0500

    feat: add technology factory

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Tags\HasTags;
 
 class Technology extends Model
 {
-    use HasTags;
+    use HasTags, HasFactory;
     protected $guarded = ['id'];
     protected $casts = [
         'tags' => 'array',
---
+++ database/factories/TechnologyFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Technology>
+ */
+class TechnologyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $az = range('a','z');
+        return [
+            'title' => fake()->words(rand(8, 30), true),
+            'case_number' => implode("", fake()->randomElements($az, rand(2,5)))."-".rand(100,99999),
+            'abstract' => fake()->words(rand(80, 300), true),
+            'applications' => fake()->words(rand(80, 300), true),
+            'advantages' => fake()->words(rand(80, 300), true),
+            'publications' => fake()->words(rand(80, 300), true),
+            'related_links' => fake()->words(rand(80, 300), true),
+        ];
+    }
+}
