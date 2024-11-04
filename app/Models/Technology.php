c66e957aae851de3377b5c7dd2686b47dfd29ef6
Michael Kimsal <michael@kimsal.com>
2024-11-04 18:06:40 -0500

    feat: make technology searchable in scout
    first pass - no tag support

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -4,13 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 use Spatie\Tags\HasTags;
 
 class Technology extends Model
 {
-    use HasTags, HasFactory;
+    use HasTags, HasFactory, Searchable;
     protected $guarded = ['id'];
     protected $casts = [
         'tags' => 'array',
     ];
+
+    public function searchableAs(): string
+    {
+        return 'tech_index2';
+    }
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => (int) $this->id,
+            'title' => $this->title,
+            'abstract' => $this->abstract,
+        ];
+    }
 }
