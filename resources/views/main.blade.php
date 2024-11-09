5364861fe449dbf263f6dea9091a1c0a4e5fd6ad
Michael Kimsal <michael@kimsal.com>
2024-11-09 16:17:37 -0500

    feat: add left hand menu

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,20 +1,31 @@
 @extends('layouts.app')
 @section('content')
-<form method="post"
-action="{{route('main.search')}}"
->
-    @csrf
-    <input type="text" name="search"
-           value="{{$term ?? ''}}"
-           class="border-2 mt-2 py-1 px-2 text-2xl "/>
+    <div class="grid grid-cols-6 gap-4">
+        <div class="col-span-1">
+            Left hand menu
 
-    <button type="submit" class="btn btn-green">Search</button>
-    @if($results)
-    @foreach($results as $result)
-        <x-tech-result :tech="$result" />
-    @endforeach
-    {{ $results->links() }}
-    @endif
 
-</form>
+        </div>
+
+        <div class="col-span-5">
+            <form method="post"
+                  action="{{route('main.search')}}"
+            >
+                @csrf
+                <input type="text" name="search"
+                       value="{{$term ?? ''}}"
+                       class="border-2 mt-2 py-1 px-2 text-2xl "/>
+
+                <button type="submit" class="btn btn-green">Search</button>
+            </form>
+
+            @if($results)
+                @foreach($results as $result)
+                    <x-tech-result :tech="$result"/>
+                @endforeach
+                {{ $results->links() }}
+            @endif
+        </div>
+
+    </div>
 @endsection
