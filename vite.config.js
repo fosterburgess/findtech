9d5f7ae455d235a69efa6b287b3999d98efe3950
Michael Kimsal <michael@kimsal.com>
2024-11-09 16:18:03 -0500

    chore: move views to vite reload section

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            reload: [
                 'resources/views/**',
-                'resources/css/app.css', 'resources/js/app.js'],
+            ],
             refresh: true,
         }),
     ],
