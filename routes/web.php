920f4b6ad69ef4ceacf8526d7592c2d6b0f0b052
Michael Kimsal <michael@kimsal.com>
2024-11-15 09:21:00 -0500

    feat: route main route to search action

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/main');
-Route::get('/main', [MainController::class, 'index']);
+Route::get('/main', [MainController::class, 'search']);
 Route::get('/addTag/{tag}', [MainController::class, 'addTag'])->name('main.addTag');
 Route::get('/removeTag/{tag}', [MainController::class, 'removeTag'])->name('main.removeTag');
 Route::any('/main/search', [MainController::class, 'search'])->name('main.search');
